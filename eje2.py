8eed3deba55d3ad1c901f1927c33fb6b3c50d584
juanalde <150602719+juanalde@users.noreply.github.com>
2023-11-19 01:52:48 +0000

    subiendo eje2

---
+++ eje2.py
@@ -0,0 +1,7 @@
+
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+
+print(f"La suma de {numero1} y {numero2} es: {suma}")
